1082f984c9627c89fa90f5bd7a509562cec0d428
Nathan G. Santos <nathangar01@gmail.com>
2025-04-29 22:58:28 -0300

    removing unnecessary comments

--- pkg/broker/consumer.go
+++ pkg/broker/consumer.go
@@ -36,7 +36,6 @@ func CreateTopicIfNotExists(topic string, servers string) error {
 
 	err = controllerConn.CreateTopics(topicConfigs...)
 	if err != nil {
-		// Se o erro for que o tópico já existe, ignoramos
 		if err.Error() != "topic already exists" {
 			return err
 		}
@@ -46,23 +45,19 @@ func CreateTopicIfNotExists(topic string, servers string) error {
 }
 
 func Consume(topics []string, servers string, msgChan chan *kafka.Message) {
-	// Criar um novo leitor Kafka
+
 	r := kafka.NewReader(kafka.ReaderConfig{
 		Brokers:  []string{servers},
 		GroupID:  "myGroup",
-		Topic:    topics[0], // kafka-go suporta apenas um tópico por leitor
-		MinBytes: 10e3,      // 10KB
-		MaxBytes: 10e6,      // 10MB
+		Topic:    topics[0],
+		MinBytes: 10e3, // 10KB
+		MaxBytes: 10e6, // 10MB
 	})
-
-	// Garantir que o leitor seja fechado quando a função terminar
 	defer r.Close()
 
-	// Loop infinito para consumir mensagens
 	for {
 		msg, err := r.ReadMessage(context.Background())
 		if err != nil {
-			// Em caso de erro, continuar tentando
 			continue
 		}
 		msgChan <- &msg
@@ -70,7 +65,6 @@ func Consume(topics []string, servers string, msgChan chan *kafka.Message) {
 }
 
 func Publish(topic string, servers string, msg []byte) error {
-	// Criar o tópico se não existir
 	err := CreateTopicIfNotExists(topic, servers)
 	if err != nil {
 		return err

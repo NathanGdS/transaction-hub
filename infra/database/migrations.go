2bafab28311bb6b92961559202a1b9a2db051501
Nathan G. Santos <nathan.santos@finnet.com.br>
2025-05-01 21:04:35 -0300

    feat: postgres impl and migrations

---
+++ infra/database/migrations.go
@@ -0,0 +1,10 @@
+package database
+
+import (
+	"github.com/NathanGdS/cali-challenge/domain"
+	"gorm.io/gorm"
+)
+
+func RunMigrations(db *gorm.DB) error {
+	return db.AutoMigrate(&domain.Transaction{})
+}
---
+++ infra/database/postgres.go
@@ -0,0 +1,48 @@
+package database
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+func NewPostgresConnection() (*gorm.DB, error) {
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+		getEnvOrDefault("DB_HOST", "localhost"),
+		getEnvOrDefault("DB_USER", "postgres"),
+		getEnvOrDefault("DB_PASSWORD", "postgres"),
+		getEnvOrDefault("DB_NAME", "cali"),
+		getEnvOrDefault("DB_PORT", "5432"),
+	)
+
+	newLogger := logger.New(
+		log.New(os.Stdout, "\r\n", log.LstdFlags),
+		logger.Config{
+			SlowThreshold:             time.Second,
+			LogLevel:                  logger.Info,
+			IgnoreRecordNotFoundError: true,
+			Colorful:                  true,
+		},
+	)
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
+		Logger: newLogger,
+	})
+	if err != nil {
+		return nil, fmt.Errorf("failed to connect to database: %v", err)
+	}
+
+	return db, nil
+}
+
+func getEnvOrDefault(key, defaultValue string) string {
+	if value := os.Getenv(key); value != "" {
+		return value
+	}
+	return defaultValue
+}
